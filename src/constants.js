39bffa2cfb6f83c7314bee589461303ce0284560
Paolo <paolo.herrera.araya@gmail.com>
2025-01-22 02:58:40 -0300

    Se agrega archivo de constantes con información profesional y elementos de navegación

---
+++ src/constants.js
@@ -0,0 +1,55 @@
+export const PROFESSIONAL_INFORMATION = {
+	name: 'Paolo',
+	lastName: 'Herrera',
+	profession: 'Ingeniero de Software',
+	presentation: 'Especializado en Desarrollo Web Fullstack',
+	email: 'paolo.herrera.araya@gmail.com',
+	github: 'https://github.com/PaoloHerrera',
+	image: {
+		url: 'photos/Paolo_01.webp',
+		alt: 'Foto de Paolo Herrera en blanco y negro y mirando a la cámara',
+	},
+	cv: {
+		name: 'Descargar CV',
+		link: '/assets/CV_Paolo_Herrera.pdf',
+	},
+}
+
+export const NAVBAR_BRAND = {
+	name: 'Paolo',
+	link: '#home',
+	last: '.',
+}
+
+export const NAVBAR_ITEMS = [
+	{
+		id: 1,
+		name: 'Home',
+		link: '#home',
+	},
+	{
+		id: 2,
+		name: 'Experiencia',
+		link: '#experience',
+	},
+	{
+		id: 3,
+		name: 'Proyectos',
+		link: '#projects',
+	},
+	{
+		id: 4,
+		name: 'Tecnologías',
+		link: '#technologies',
+	},
+	{
+		id: 5,
+		name: 'Sobre Mí',
+		link: '#about',
+	},
+	{
+		id: 6,
+		name: 'Contacto',
+		link: '#contact',
+	},
+]
