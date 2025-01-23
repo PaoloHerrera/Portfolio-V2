ee8df76e05b57480217f32a35605b40c49efc999
Paolo <paolo.herrera.araya@gmail.com>
2025-01-23 00:22:54 -0300

    Se agrega el color white y blue al proyecto

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,8 @@ export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		colors: {
+			white: '#ebecf3',
+			blue: '#0d1b2a',
 			primary: '#111',
 			secondary: '#0aff9d',
 			tertiary: '#ff8a5c',
