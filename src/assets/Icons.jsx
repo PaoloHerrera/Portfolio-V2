be6f123b33578ed6e4b299593623a40887a8c066
Paolo <paolo.herrera.araya@gmail.com>
2025-01-23 03:45:51 -0300

    Se agrega el ícono de Menu y Cerrar Menu

--- src/assets/Icons.jsx
+++ src/assets/Icons.jsx
@@ -45,3 +45,39 @@ export const CalendarIcon = () => (
 		/>
 	</svg>
 )
+
+export const MenuIcon = () => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		className="h-6 w-6"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke="currentColor"
+		strokeWidth={2}
+	>
+		<title>Menu</title>
+		<path
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			d="M4 6h16M4 12h16M4 18h16"
+		/>
+	</svg>
+)
+
+export const CloseIcon = () => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		className="h-6 w-6"
+		fill="none"
+		viewBox="0 0 24 24"
+		stroke="currentColor"
+		strokeWidth={2}
+	>
+		<title>Cerrar</title>
+		<path
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			d="M6 18L18 6M6 6l12 12"
+		/>
+	</svg>
+)
