6cf99c696d063b1427459827e151d731ce0b72a0
Paolo <paolo.herrera.araya@gmail.com>
2025-01-23 17:23:13 -0300

    Migración a Tailwind 4

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,6 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		colors: {
-			white: '#ebecf3',
-			blue: '#0d1b2a',
-			primary: '#111',
-			secondary: '#0aff9d',
-			tertiary: '#ff8a5c',
-		},
 		extend: {},
 	},
 	plugins: [],
