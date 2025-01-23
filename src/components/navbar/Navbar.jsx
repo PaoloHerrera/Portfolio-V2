07eaf6fcf4fe41fc54c39f89bbb7f2a430b2cc2b
Paolo <paolo.herrera.araya@gmail.com>
2025-01-23 17:23:41 -0300

    Se agrega un header para estructurar mejor el navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -13,22 +13,24 @@ export default function Navbar() {
 	const { isMenuOpen, setIsMenuOpen } = useMenu()
 
 	return (
-		<nav className="flex justify-center items-center">
-			<div className="nav-list-container flex justify-between">
-				<NavbarBrand item={NAVBAR_BRAND} />
-				<div className="hidden lg:flex">
-					<NavbarLinks items={NAVBAR_ITEMS} />
+		<header>
+			<nav className="flex justify-center items-center">
+				<div className="nav-list-container flex justify-between items-center">
+					<NavbarBrand item={NAVBAR_BRAND} />
+					<div className="hidden lg:flex">
+						<NavbarLinks items={NAVBAR_ITEMS} />
+					</div>
+					{!isMenuOpen && <ToggleButton setIsMenuOpen={setIsMenuOpen} />}
+					{isMenuOpen && (
+						<>
+							<CloseButton setIsMenuOpen={setIsMenuOpen} />
+							<Sidebar>
+								<NavbarLinks items={NAVBAR_ITEMS} />
+							</Sidebar>
+						</>
+					)}
 				</div>
-				{!isMenuOpen && <ToggleButton setIsMenuOpen={setIsMenuOpen} />}
-				{isMenuOpen && (
-					<>
-						<CloseButton setIsMenuOpen={setIsMenuOpen} />
-						<Sidebar>
-							<NavbarLinks items={NAVBAR_ITEMS} />
-						</Sidebar>
-					</>
-				)}
-			</div>
-		</nav>
+			</nav>
+		</header>
 	)
 }
