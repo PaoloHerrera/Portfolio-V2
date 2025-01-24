3dc87a0caecb06b791e8ea5fd3898b90689d1f95
Paolo <paolo.herrera.araya@gmail.com>
2025-01-24 02:12:47 -0300

    Se cambia el size del texto de los títulos cuando se ve en dispositivos más pequeños

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -3,7 +3,7 @@ import './SectionTitle.css'
 export default function SectionTitle({ title }) {
 	return (
 		<div className="flex flex-row justify-center items-center w-full h-full">
-			<h2 className="font-bold text-nowrap text-5xl">
+			<h2 className="font-bold text-nowrap md:text-5xl text-3xl">
 				{title}
 				<span className="text-secondary">.</span>
 			</h2>
