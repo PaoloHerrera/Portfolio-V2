f6484b525af4939640b66987d135896e6a9a1093
Paolo <paolo.herrera.araya@gmail.com>
2025-01-23 21:59:22 -0300

    Se ajusta la sección de experiencia

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,16 +1,17 @@
 import SectionTitle from '../SectionTitle'
 import { ExperienceTimeline } from './ExperienceComponents'
 import { EXPERIENCE_ITEMS } from '../../constants'
+import './Experience.css'
 
 export default function Experience() {
 	return (
 		<section
 			id="experience"
-			className="h-full w-full pt-32 grid grid-rows-12 gap-8"
+			className="experience-container w-full pt-32 grid grid-rows-4 gap-8"
 		>
 			<SectionTitle title="Experiencia" className="row-span-1" />
 
-			<div className="w-full row-span-11 relative">
+			<div className="w-full row-span-3 relative">
 				<ExperienceTimeline items={EXPERIENCE_ITEMS} />
 			</div>
 		</section>
