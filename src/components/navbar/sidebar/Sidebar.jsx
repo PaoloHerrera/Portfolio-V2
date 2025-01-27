976ae34f2940086356cd0100478e41b38623834a
Paolo <paolo.herrera.araya@gmail.com>
2025-01-27 14:09:23 -0300

    Se renombra y reorganiza el componente Sidebar, simplificando el CSS y extrayendo variantes de animación a un nuevo archivo para mejorar la estructura y la legibilidad del código.

--- src/components/navbar/sidebar/Sidebar.jsx
+++ src/components/navbar/sidebar/Sidebar.jsx
@@ -1,27 +1,8 @@
 import './Sidebar.css'
 import { motion } from 'motion/react'
+import { variants } from './variants'
 
 export default function Sidebar({ children, isMenuOpen }) {
-	const variants = {
-		open: {
-			clipPath: 'circle(200% at right 0)',
-			transition: {
-				type: 'spring',
-				stiffness: 20,
-				restDelta: 2,
-				damping: 20,
-			},
-		},
-		close: {
-			clipPath: 'circle(0% at right 0)',
-			transition: {
-				delay: 0.3,
-				type: 'spring',
-				stiffness: 400,
-				damping: 40,
-			},
-		},
-	}
 
 	return (
 		<motion.aside
---
+++ src/components/navbar/sidebar/variants.js
@@ -0,0 +1,20 @@
+export const variants = {
+	open: {
+		clipPath: 'circle(200% at right 0)',
+		transition: {
+			type: 'spring',
+			stiffness: 20,
+			restDelta: 2,
+			damping: 20,
+		},
+	},
+	close: {
+		clipPath: 'circle(0% at right 0)',
+		transition: {
+			delay: 0.3,
+			type: 'spring',
+			stiffness: 400,
+			damping: 40,
+		},
+	},
+}
