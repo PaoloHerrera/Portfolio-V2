b8bd197c77d4dc03e5efbad7b8f755a7c16465ac
Paolo <paolo.herrera.araya@gmail.com>
2025-01-28 19:22:25 -0300

    Se refactoriza el componente ExperienceCard para integrar SpotlightCard, mejorando la presentación y la interactividad. Se ajustan estilos y se simplifica la estructura del código.

--- src/components/Experience/ExperienceCard/ExperienceCard.jsx
+++ src/components/Experience/ExperienceCard/ExperienceCard.jsx
@@ -2,17 +2,13 @@ import styles from './ExperienceCard.module.css'
 import { List } from '@/components/Shared/List/List.jsx'
 import { CalendarIcon } from '@/components/Shared/Icons/Icons.jsx'
 import { Title } from '@/components/Shared/Title/Title.jsx'
+import { SpotlightCard } from '@/components/Shared/SpotlightCard/SpotlightCard.jsx'
 
 const ExperienceHeader = ({ item }) => {
 	return (
 		<div className="flex flex-col md:flex-row md:justify-between gap-4">
 			<Title>{item.company}</Title>
-			<span className="font-bold text-md flex items-center">
-				{item.date}
-				<span className={styles.timelineIcon}>
-					<CalendarIcon />
-				</span>
-			</span>
+			<span className="font-bold text-md flex items-center">{item.date}</span>
 		</div>
 	)
 }
@@ -33,13 +29,19 @@ export const ExperienceCard = ({ item, position }) => {
 		<article
 			className={`${styles.experienceCard} ${styles[position]} w-full lg:w-1/2`}
 		>
-			<div className="border border-black bg-black rounded-lg min-h-[320px] p-8 flex flex-col gap-5">
+			<SpotlightCard
+				spotlightColor="rgba(255, 255, 255, 0.25)"
+				className="min-h-[320px] p-8 flex flex-col gap-5"
+			>
 				<ExperienceHeader item={item} />
 
 				<ExperienceSubheader item={item} />
 
 				<List items={item.description} />
-			</div>
+			</SpotlightCard>
+			<span className={styles.timelineIcon}>
+				<CalendarIcon />
+			</span>
 		</article>
 	)
 }
