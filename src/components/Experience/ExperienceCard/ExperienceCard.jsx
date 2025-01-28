f0f6f037c023681cc7d52d1ffcc51694b7e28fbe
Paolo <paolo.herrera.araya@gmail.com>
2025-01-28 02:00:22 -0300

    Se refactoriza el componente ExperienceCard, integrando ExperienceHeader y ExperienceSubheader como funciones internas. Se eliminan los archivos de los componentes obsoletos y se ajustan estilos para mejorar la presentación.

--- src/components/Experience/ExperienceCard/ExperienceCard.jsx
+++ src/components/Experience/ExperienceCard/ExperienceCard.jsx
@@ -1,8 +1,32 @@
 import styles from './ExperienceCard.module.css'
 import { List } from '@/components/Shared/List/List.jsx'
 import { CalendarIcon } from '@/components/Shared/Icons/Icons.jsx'
-import { ExperienceHeader } from './ExperienceHeader.jsx'
-import { ExperienceSubheader } from './ExperienceSubheader.jsx'
+import { Title } from '@/components/Shared/Title/Title.jsx'
+
+const ExperienceHeader = ({ item }) => {
+	return (
+		<div className="flex flex-col md:flex-row md:justify-between gap-4">
+			<Title>{item.company}</Title>
+			<span className="font-bold text-md flex items-center">
+				{item.date}
+				<span className={styles.timelineIcon}>
+					<CalendarIcon />
+				</span>
+			</span>
+		</div>
+	)
+}
+
+const ExperienceSubheader = ({ item }) => {
+	return (
+		<div className="flex flex-col md:flex-row md:justify-between mb-5 gap-3">
+			<Title variant="secondary">{item.role}</Title>
+			<span className="font-bold text-md flex items-center">
+				{item.city}, {item.country}
+			</span>
+		</div>
+	)
+}
 
 export const ExperienceCard = ({ item, position }) => {
 	return (
@@ -10,7 +34,7 @@ export const ExperienceCard = ({ item, position }) => {
 			className={`${styles.experienceCard} ${styles[position]} w-full lg:w-1/2`}
 		>
 			<div className="border border-black bg-black rounded-lg min-h-[320px] p-8 flex flex-col gap-5">
-				<ExperienceHeader item={item} timelineIcon={<CalendarIcon />} />
+				<ExperienceHeader item={item} />
 
 				<ExperienceSubheader item={item} />
 
