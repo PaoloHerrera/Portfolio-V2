3f6eee520b1d59485e7926d0a2645961a36d49e6
Paolo <paolo.herrera.araya@gmail.com>
2025-01-29 02:29:48 -0300

    Se añade el componente Button para mejorar la reutilización de botones en la aplicación. Se ajusta el estilo para incluir un cursor pointer y se define un nuevo estilo secundario.

--- src/components/Shared/Button/Button.jsx
+++ src/components/Shared/Button/Button.jsx
@@ -11,9 +11,21 @@ export const ButtonLink = ({
 			download={isDownload}
 			target={isDownload ? undefined : '_blank'}
 			rel={isDownload ? undefined : 'noreferrer'}
-			className={`${styles.button} ${styles[variant]}`}
+			className={`${styles.button} ${styles[variant]} ml-2`}
 		>
 			{children}
 		</a>
+	)
+}
+
+export const Button = ({ children, variant = 'primary', type, onClick }) => {
+	return (
+		<button
+			className={`${styles.button} ${styles[variant]}`}
+			type={type}
+			onClick={onClick}
+		>
+			{children}
+		</button>
 	)
 }
