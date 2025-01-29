64b286f3234da09039454df7b9b6d0fb75a52012
Paolo <paolo.herrera.araya@gmail.com>
2025-01-28 23:15:37 -0300

    Se crea el componente SkillCard para mostrar habilidades con iconos y se organiza la información de habilidades en un nuevo archivo skills.js. Se actualiza la página Skills para utilizar SkillCard en lugar de componentes individuales.

---
+++ src/components/Skills/SkillCard.jsx
@@ -0,0 +1,27 @@
+import { SpotlightCard } from '@/components/Shared/SpotlightCard/SpotlightCard.jsx'
+
+const SkillContent = ({ skills }) => {
+	return (
+		<div className="w-full flex flex-row gap-14 justify-center flex-wrap h-full items-center">
+			{skills.map((skill) => (
+				<div className="flex flex-col gap-2 items-center" key={skill.id}>
+					<skill.icon height="4em" width="4em" />
+					<span className="text-sm">{skill.name}</span>
+				</div>
+			))}
+		</div>
+	)
+}
+
+export const SkillCard = ({ name, skills }) => {
+	return (
+		<article className="w-full relative min-h-[300px]">
+			<SpotlightCard className="h-full">
+				<div className="flex flex-col gap-14 items-center justify-center h-full">
+					<h3 className="font-bold lg:text-4xl text-2xl">{name}</h3>
+					<SkillContent skills={skills} />
+				</div>
+			</SpotlightCard>
+		</article>
+	)
+}
