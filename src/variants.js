55e215eb3e05b2bba80d26c38062c26854422a80
Paolo <paolo.herrera.araya@gmail.com>
2025-01-29 19:23:48 -0300

    Se añaden variantes de animación en variants.js para mejorar la transición de elementos.

---
+++ src/variants.js
@@ -0,0 +1,16 @@
+export const leftVariants = {
+	initial: { opacity: 0, x: -500 },
+	animate: {
+		opacity: 1,
+		x: 0,
+		transition: { duration: 0.5, ease: 'easeInOut' },
+	},
+}
+
+export const opacityVariants = {
+	initial: { opacity: 0 },
+	animate: {
+		opacity: 1,
+		transition: { duration: 0.5, ease: 'easeInOut' },
+	},
+}
