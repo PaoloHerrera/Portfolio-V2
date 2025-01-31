6a3205f222eed4c744cc3d869f9d79c89577249b
Paolo <paolo.herrera.araya@gmail.com>
2025-01-31 03:32:00 -0300

    Optimiza el componente CalendarIcon al simplificar el path SVG y eliminar la redundancia en la propiedad fill.

--- src/components/Shared/Icons/Icons.jsx
+++ src/components/Shared/Icons/Icons.jsx
@@ -20,12 +20,10 @@ export const CalendarIcon = () => (
 		height="24"
 		width="24"
 		xmlns="http://www.w3.org/2000/svg"
+		fill="currentColor"
 	>
 		<title>Calendario</title>
-		<path
-			fill="currentColor"
-			d="M20 3h-1V1h-2v2H7V1H5v2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 18H4V9h16v13zm0-14H4V5h16v3z"
-		/>
+		<path d="M20 3h-1V1h-2v2H7V1H5v2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 18H4V9h16v13zm0-14H4V5h16v3z" />
 	</svg>
 )
 
