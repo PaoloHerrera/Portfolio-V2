9913a41442994e211e89b33562a35033b3c9d6a2
Paolo <paolo.herrera.araya@gmail.com>
2025-02-01 18:25:43 -0300

    Se cambia la estética de los proyectos y se elimina el CSS de ProyectCard

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -11,20 +11,15 @@ export const ProjectCard = ({ item }) => {
 
 	return (
 		<motion.article
-			className={`${styles.projectCard} h-[400px] min-w-[300px] flex flex-col gap-4 rounded-md relative shadow-xl hover:opacity-80 hover:scale-105 transition-all duration-300 ease-in-out`}
+			className={`${styles.projectCard} flex flex-col gap-4 w-full`}
 			initial={{ scale: 0.5, y: 50 }}
 			whileInView={{ scale: 1, y: 0 }}
 			transition={{ duration: 0.3, ease: 'easeInOut' }}
 			viewport={{ once: false, margin: '0px 0px -110px 0px' }}
 		>
-			<div className="p-2 h-full">
-				<img
-					src={item.image}
-					alt={item.alt}
-					className="w-full object-cover h-full"
-				/>
-				<div className="absolute top-0 left-0 opacity-0 hover:opacity-100 p-10">
-					<header className="flex flex-col gap-4">
+			<div className="flex flex-col-reverse md:flex-row gap-10">
+				<div className="w-full md:w-1/2 flex flex-col md:gap-20 gap-10">
+					<header className="flex flex-col gap-4 lg:gap-6">
 						<h3 className="font-bold text-2xl md:text-4xl text-center">
 							{item.name}
 						</h3>
@@ -39,19 +34,27 @@ export const ProjectCard = ({ item }) => {
 					</header>
 
 					<div>
-						<p className="text-md opacity-90">{item.description}</p>
+						<p className="opacity-90 md:text-lg">{item.description}</p>
 					</div>
 
-					<footer className="flex flex-row gap-5 justify-center">
+					<footer className="flex flex-wrap gap-5 justify-center">
 						<ButtonLink href={item.github} variant="secondary">
-							<Github height="2rem" width="2rem" />
-							Code
+							<Github height="1.5rem" width="1.5rem" />
+							<span className="hidden sm:inline">Code</span>
 						</ButtonLink>
-						<ButtonLink href={item.link} variant="secondary">
-							<LinkIcon height="2rem" width="2rem" />
-							Preview
+						<ButtonLink href={item.link} variant="secondary" className="">
+							<LinkIcon height="1.5rem" width="1.5rem" />
+							<span className="hidden sm:inline">Preview </span>
 						</ButtonLink>
 					</footer>
+				</div>
+
+				<div className="aspect-16/9 md:w-1/2 w-full">
+					<img
+						src={item.image}
+						alt={item.alt}
+						className="w-full object-cover h-full"
+					/>
 				</div>
 			</div>
 		</motion.article>
--- src/pages/ProjectsPage/Projects.jsx
+++ src/pages/ProjectsPage/Projects.jsx
@@ -1,16 +1,13 @@
 import { PageTitle } from '@/pages/PageTitle.jsx'
 import { PROJECTS_ITEMS } from '@/constants.js'
 import { ProjectCard } from '@/components/Projects/ProjectCard.jsx'
-import { useRef } from 'react'
 
 export const Projects = ({ title }) => {
-	const ref = useRef()
-
 	return (
 		<section id="projects" className="projects-container">
 			<PageTitle title={title} />
 
-			<div className="flex flex-wrap gap-2 mt-10 justify-center" ref={ref}>
+			<div className="flex flex-wrap mt-10 justify-center gap-40">
 				{PROJECTS_ITEMS.map((item) => (
 					<ProjectCard key={item.id} item={item} />
 				))}
