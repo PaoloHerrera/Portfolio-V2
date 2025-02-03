85dce0abf05854fcb96996f14b7603266663e6f8
Paolo <paolo.herrera.araya@gmail.com>
2025-02-03 01:17:32 -0300

    Añade un hook personalizado para la traducción de textos en la aplicación

---
+++ src/hooks/useTranslation.js
@@ -0,0 +1,25 @@
+import { useContext } from 'react'
+import { LangContext } from '@/context/LangContext.js'
+import { translations } from '@/i18n/translations.js'
+
+export const useTranslation = () => {
+	const { language } = useContext(LangContext)
+
+	// Función para traducir el texto. Recibe el key de la traducción en el objeto de traducciones y devuelve el texto traducido
+	const translate = (key) => {
+		const keys = key.split('.')
+		let value = translations[language]
+
+		for (const k of keys) {
+			value = value[k]
+
+			if (!value) {
+				break
+			}
+		}
+
+		return value || translations.en[key]
+	}
+
+	return { translate }
+}
