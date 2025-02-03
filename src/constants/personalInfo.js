fef67849653b0df606f4058bbd5bf3e3a5721f93
Paolo <paolo.herrera.araya@gmail.com>
2025-02-03 01:20:08 -0300

    Añade constantes de información personal en inglés y español

---
+++ src/constants/personalInfo.js
@@ -0,0 +1,38 @@
+export const PERSONAL_INFO = {
+	es: {
+		name: 'Paolo',
+		lastName: 'Herrera',
+		profession: 'Ingeniero de Software',
+		description: 'Especializado en Desarrollo Web Fullstack',
+		email: 'paolo.herrera.araya@gmail.com',
+		github: 'https://github.com/PaoloHerrera',
+		city: 'Santiago',
+		country: 'Chile',
+		image: {
+			url: 'photos/Paolo_01.webp',
+			alt: 'Foto de Paolo Herrera en blanco y negro y mirando a la cámara',
+		},
+		cv: {
+			name: 'Descargar CV',
+			link: '/assets/CV_Paolo_Herrera.pdf',
+		},
+	},
+	en: {
+		name: 'Paolo',
+		lastName: 'Herrera',
+		profession: 'Software Engineer',
+		description: 'Specialized in Fullstack Web Development',
+		email: 'paolo.herrera.araya@gmail.com',
+		github: 'https://github.com/PaoloHerrera',
+		city: 'Santiago',
+		country: 'Chile',
+		image: {
+			url: 'photos/Paolo_01.webp',
+			alt: 'Photo of Paolo Herrera in black and white and looking at the camera',
+		},
+		cv: {
+			name: 'Download CV',
+			link: '/assets/CV_Paolo_Herrera.pdf',
+		},
+	},
+}
