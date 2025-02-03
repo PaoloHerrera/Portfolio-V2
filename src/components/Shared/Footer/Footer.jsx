68a00be839a47d447398b4795d4e7112d894a03a
Paolo <paolo.herrera.araya@gmail.com>
2025-02-03 01:21:36 -0300

    Refactor Footer para gestionar traducciones y actualizar enlaces dinámicamente

--- src/components/Shared/Footer/Footer.jsx
+++ src/components/Shared/Footer/Footer.jsx
@@ -2,22 +2,28 @@ import { IconLink } from '@/components/Shared/IconLink/IconLink.jsx'
 import { Github } from '@/components/Shared/Icons/Tools/Github.jsx'
 import { MailIcon } from '@/components/Shared/Icons/Icons.jsx'
 
+import { useTranslation } from '@/hooks/useTranslation.js'
+
 export const Footer = () => {
+	const { translate } = useTranslation()
+
+	const footer = translate('footer')
+
 	return (
-		<footer className="opacity-80  mt-16 w-full mx-auto mb-10">
+		<footer className="opacity-90 mt-16 w-full mx-auto mb-10">
 			<div className="container mx-auto flex flex-col md:flex-row justify-between items-center max-w-[1200px]">
 				<div className="text-center md:text-left mb-4 md:mb-0">
-					<p className="text-sm font-bold">&copy; 2025 Paolo Herrera.</p>
-					<p className="text-sm text-secondary">Ingeniero de Software</p>
-					<p className="text-xs opacity-80">
-						Hecho con React, TailwindCSS y con ❤️
+					<p className="text-sm font-bold">
+						&copy; {footer.year} {footer.name} {footer.lastName}
 					</p>
+					<p className="text-sm text-secondary">{footer.profession}</p>
+					<p className="text-xs opacity-80">{footer.legend}</p>
 				</div>
 				<div className="flex gap-4">
-					<IconLink href="https://github.com/paolo-herrera" target="_blank">
+					<IconLink href={footer.github}>
 						<Github />
 					</IconLink>
-					<IconLink href="mailto:0LHtP@example.com" target="_blank">
+					<IconLink href={`mailto:${footer.email}`}>
 						<MailIcon height="1rem" width="1rem" />
 					</IconLink>
 				</div>
