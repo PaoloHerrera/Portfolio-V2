2b79445a502e2d9babae3ba954c33210a1a3be29
Paolo <paolo.herrera.araya@gmail.com>
2025-02-03 22:23:15 -0300

    Actualiza el color del texto en el componente Chip a blanco y elimina el borde para mejorar la estética visual

--- src/components/Shared/Chip/Chip.jsx
+++ src/components/Shared/Chip/Chip.jsx
@@ -3,7 +3,7 @@ import styles from './Chip.module.css'
 export const Chip = ({ children, className = '' }) => {
 	return (
 		<span
-			className={`${className} ${styles.chip} flex flex-row gap-2 text-xs items-center`}
+			className={`${className} ${styles.chip} flex flex-row gap-2 text-xs items-center text-white`}
 		>
 			{children}
 		</span>
