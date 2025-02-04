cb9a222c829d5b5aafb8685d60294eb945deb32b
Paolo <paolo.herrera.araya@gmail.com>
2025-02-04 19:15:40 -0300

    Mejora la estructura y el estilo del componente HeroImage para una mejor responsividad y presentación de la imagen.

--- src/components/Hero/HeroImage/HeroImage.jsx
+++ src/components/Hero/HeroImage/HeroImage.jsx
@@ -5,17 +5,19 @@ import { opacityVariants as variants } from '@/variants.js'
 export const HeroImage = ({ image }) => {
 	return (
 		<motion.article
-			className={`${styles.heroImageContainer} max-w-[300px] md:max-w-full`}
+			className="relative flex items-center justify-center max-w-[300px] md:max-w-[500px] w-full h-full"
 			variants={variants}
 			initial="initial"
 			animate="animate"
 		>
-			<img
-				src={image.url}
-				alt={image.alt}
-				className={`${styles.heroImage} sm:w-full w-auto sm:h-auto`}
-				loading="lazy"
-			/>
+			<div className="aspect-3/4 relative w-full">
+				<img
+					src={image.url}
+					alt={image.alt}
+					className={`${styles.heroImage} object-cover rounded-lg absolute z-10`}
+					loading="lazy"
+				/>
+			</div>
 			<div className={styles.verticalColor} />
 		</motion.article>
 	)
