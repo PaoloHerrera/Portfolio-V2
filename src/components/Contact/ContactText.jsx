b34be6a053006044285fd2a82ef075eaf618ef57
Paolo <paolo.herrera.araya@gmail.com>
2025-02-06 20:31:31 -0300

    Reemplaza el componente Text por etiquetas HTML en ContactText para mejorar la semántica y la accesibilidad del contenido.

--- src/components/Contact/ContactText.jsx
+++ src/components/Contact/ContactText.jsx
@@ -1,22 +1,21 @@
-import { Text } from '@/components/Shared/Text/Text.jsx'
 import { motion } from 'motion/react'
 import { textVariants as variants } from './variants.js'
 
 export const ContactText = ({ header, description, email, address }) => {
 	return (
 		<motion.div
-			className="max-w-[400px] flex flex-col gap-10"
+			className="flex flex-col gap-10 max-w-[400px] w-full"
 			variants={variants}
 			initial="initial"
 			whileInView="animate"
 			viewport={{ once: true }}
 		>
 			<h3 className="font-bold md:text-4xl text-2xl">{header}</h3>
-			<Text>{description}</Text>
-			<Text variant="secondary">📧 {email}</Text>
-			<Text>
+			<p className="opacity-90 md:text-lg ">{description}</p>
+			<span className="font-bold md:text-lg text-secondary">📧 {email}</span>
+			<span className="font-bold md:text-lg">
 				🌎 {address.city}, {address.country}
-			</Text>
+			</span>
 		</motion.div>
 	)
 }
