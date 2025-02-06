a693760b8eff4c61dd872f655674483ef8f747b9
Paolo <paolo.herrera.araya@gmail.com>
2025-02-06 20:31:01 -0300

    Elimina los componentes Text y Title junto con sus estilos para simplificar la estructura del proyecto.

--- src/components/Shared/Text/Text.jsx
+++
@@ -1,5 +0,0 @@
-import styles from './Text.module.css'
-
-export const Text = ({ children, variant = 'primary' }) => {
-	return <p className={`${styles[variant]} text-lg`}>{children}</p>
-}
--- src/components/Shared/Title/Title.jsx
+++
@@ -1,5 +0,0 @@
-import styles from './Title.module.css'
-
-export const Title = ({ children, variant = 'primary' }) => {
-	return <h3 className={`font-bold ${styles[variant]}`}>{children}</h3>
-}
