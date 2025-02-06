fb31fe364a5397e9e9c8e72e3e72884ef7231bf6
Paolo <paolo.herrera.araya@gmail.com>
2025-02-06 20:31:24 -0300

    Ajusta el estilo de los elementos de la lista para mejorar la legibilidad al cambiar la clase de opacidad a fuente media.

--- src/components/Shared/List/List.jsx
+++ src/components/Shared/List/List.jsx
@@ -2,7 +2,7 @@ export const List = ({ items }) => {
 	return (
 		<ul className="flex flex-col gap-3">
 			{items.map((item) => (
-				<li key={item} className="list-disc list-inside text-md opacity-90">
+				<li key={item} className="list-disc list-inside text-md font-medium">
 					{item}
 				</li>
 			))}
