d98a927ec00df7749c9bd8de236d702564ca9042
Paolo <paolo.herrera.araya@gmail.com>
2025-02-07 01:37:34 -0300

    Ajusta el diseño del componente HeroImage para mejorar la disposición y responsividad de la imagen, optimizando las clases y estilos utilizados.

--- src/components/Hero/HeroImage/HeroImage.jsx
+++ src/components/Hero/HeroImage/HeroImage.jsx
@@ -5,19 +5,19 @@ import { opacityVariants as variants } from '@/variants.js'
 export const HeroImage = ({ image }) => {
 	return (
 		<motion.article
-			className="relative flex items-center justify-center max-w-[300px] md:max-w-[500px] w-full h-full"
+			className="flex items-center justify-center max-w-[300px] md:max-w-[400px] w-full h-full relative"
 			variants={variants}
 			initial="initial"
 			animate="animate"
 		>
-			<div className="aspect-3/4 flex justify-center">
+			<div className="aspect-3/4 flex justify-center max-h-full relative">
 				<img
 					src={image.url}
 					alt={image.alt}
-					className={`${styles.heroImage} w-60 sm:w-72 object-cover aspect-square rounded-lg md:w-full z-10`}
+					className={`${styles.heroImage} object-cover rounded-lg z-10`}
 					loading="lazy"
 				/>
-				<div className="w-1/2 absolute h-full top-0 bg-secondary" />
+				<div className="absolute bg-secondary bottom-0 h-screen w-1/2" />
 			</div>
 		</motion.article>
 	)
